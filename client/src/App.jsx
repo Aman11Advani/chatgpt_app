5eb6f8141d2b61bff6613830abe3b06c5f8f1f3e
Aman11Advani <aman11advani@gmail.com>
2024-05-21 01:22:55 +0530

    first

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,6 @@ function App() {
     const fetchBotResponse = async () => {
         const { data } = await axios.post(
             "https://chatgpt-ai-app-od21.onrender.com",
-            // "http://localhost:5173",
             { input },
             {
                 headers: {
